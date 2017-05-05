e98794d0aa3ec3da78140622a94594df5cea73f7
TerAleS <terales@users.noreply.github.com>
2017-05-05 08:47:03 +0300

    extract rendering into drawScene function

--- _src/_entry.js
+++ _src/_entry.js
@@ -47,55 +47,69 @@ const positions = [
 
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
 
+drawScene()
+
 /**
  * Rendering
  */
 
-// Set canvas and viewport size
-const viewport = canvasResize(gl.canvas)
-gl.viewport(0, 0, viewport.width, viewport.height)
+// https://webglfundamentals.org/webgl/lessons/webgl-2d-translation.html
+function drawScene () {
+  // Set canvas and viewport size
+  const viewport = canvasResize(gl.canvas)
+  gl.viewport(0, 0, viewport.width, viewport.height)
 
-// Clear the canvas
-gl.clearColor(0, 0, 0, 0)
-gl.clear(gl.COLOR_BUFFER_BIT)
+  // Set up hardcover dimensions, position and color
+  const hardcoverWidth = hardcoverWidthClipspace(viewport.width, viewport.height)
+  const hardcoverHeight = 2 // Full canvas width
+  const hardcoverTranslation = [
+    -hardcoverWidth / 2, // move left from screen center by half of hardcover width
+    0.8 // from http://artgorbunov.ru/projects/book-ui/
+  ]
+  // RGBA(254, 116, 40, 1) - color of http://artgorbunov.ru/projects/book-ui/
+  const hardcoverColor = new Float32Array([254/255, 116/255, 40/255, 1])
 
-// Tell canvas to use our program (pair of shaders)
-gl.useProgram(program)
+  // Clear the canvas
+  gl.clearColor(0, 0, 0, 0)
+  gl.clear(gl.COLOR_BUFFER_BIT)
 
-// tell WebGL how to take data from the buffer we setup above
-// and supply it to the attribute in the shader
-gl.enableVertexAttribArray(positionAttributeLocation)
+  // Tell canvas to use our program (pair of shaders)
+  gl.useProgram(program)
 
-// Bind the position buffer.
-gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+  // tell WebGL how to take data from the buffer we setup above
+  // and supply it to the attribute in the shader
+  // Turn on the attribute
+  gl.enableVertexAttribArray(positionAttributeLocation)
 
-// Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-const size = 2          // 2 components per iteration
-const type = gl.FLOAT   // the data is 32bit floats
-const normalize = false // don't normalize the data
-const stride = 0        // 0 = move forward size * sizeof(type) each iteration
-                        // to get the next position
-const offset = 0        // start at the beginning of the buffer
-gl.vertexAttribPointer(
-  positionAttributeLocation, size, type, normalize, stride, offset)
+  // Bind the position buffer.
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
 
-// Draw
-const width = hardcoverWidthClipspace(viewport.width, viewport.height)
+  rectangleAdd({
+    gl,
+    x: hardcoverTranslation[0], 
+    y: hardcoverTranslation[1],
+    width: hardcoverWidth,
+    height: hardcoverHeight
+  })
 
-rectangleAdd({
-  gl,
-  x: -width / 2, // move left from screen center by half of width
-  y: 0.8,
-  width: width,
-  height: 2
-})
+  // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+  const size = 2          // 2 components per iteration
+  const normalize = false // don't normalize the data
+  const bufferOffset = 0  // start at the beginning of the buffer
+  const type = gl.FLOAT   // the data is 32bit floats
+  const stride = 0        // 0 = move forward size * sizeof(type) each iteration
+                          // to get the next position
+  gl.vertexAttribPointer(
+    positionAttributeLocation, size, type, normalize, stride, bufferOffset)
 
- // Set color
- // RGB(254, 116, 40) - color of http://artgorbunov.ru/projects/book-ui/
- gl.uniform4f(colorUniformLocation, 254/255, 116/255, 40/255, 1)
+  // Set color
+  gl.uniform4fv(colorUniformLocation, hardcoverColor)
 
-const primitiveType = gl.TRIANGLES
-const count = 6
-gl.drawArrays(primitiveType, offset, count)
+  // Draw passet atributes
+  const primitiveType = gl.TRIANGLES
+  const drawOffset = 0
+  const count = 6
+  gl.drawArrays(primitiveType, drawOffset, count)
 
-console.log('Drawn')
+  console.log('drawn')
+}
